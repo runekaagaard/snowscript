78483734aa1ac2d91fc4361c97f87a9d30f20b6e
Rune Kaagaard <rumi.kg@gmail.com>
2010-03-16 18:20:36 +0000

    factorial example: Fixing incorectly named function call and that the procedural version works for n=0.
    git-svn-id: http://php-snow.googlecode.com/svn/trunk@37 037d5886-2c84-11df-b580-1f2e1e9a714e

--- examples/factorial.php
+++ examples/factorial.php
@@ -7,9 +7,13 @@
  * @return int
  */
 function fac1($n) {
-    if ($n == 0) return 1;
+    if ($n == 0) { 
+        return 1;
+    }
     return $n * fac1($n-1);
 }
+var_dump(fac1(0));
+var_dump(fac1(1));
 var_dump(fac1(10));
 
 /**
@@ -20,9 +24,14 @@ var_dump(fac1(10));
  * @return int
  */
 function fac2($n) {
+    if ($n == 0) {
+        return 1;
+    }
     for ($i=$n-1;$i>1;--$i) {
         $n *= $i;
     }
     return $n;
 }
+var_dump(fac2(0));
+var_dump(fac2(1));
 var_dump(fac2(10));
