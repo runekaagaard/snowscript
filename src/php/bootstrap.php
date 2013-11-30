83c0249085def40dbb0b550a420fdfe9f6ec58d2
Rune Kaagaard <rumi.kg@gmail.com>
2013-11-30 19:10:39 +0100

    Remember to add changes.

--- src/php/bootstrap.php
+++ src/php/bootstrap.php
@@ -3,4 +3,5 @@
 require dirname(__FILE__) . '/Snowscript/Autoloader.php';
 require dirname(__FILE__) . '/Snowscript/Visitors.php';
 require dirname(__FILE__) . '/Snowscript/snowscript.php';
+require dirname(__FILE__) . '/../../stdlib/bootstrap.php';
 Snowscript_Autoloader::register();
